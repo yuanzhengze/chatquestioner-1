fa08ea2cb7f5f722f4197ad06e063dd85f6b3597
zaynyuan <zaynyuan@tencent.com>
2026-06-08 20:20:07 +0800

    feat(rhythm): data contracts (Note/Chart/RhythmState/RhythmDef)

--- packages/rhythm/src/index.ts
+++ packages/rhythm/src/index.ts
@@ -1,1 +1,15 @@
-export {};
+export type {
+  NoteType,
+  FlickDir,
+  Judgement,
+  Note,
+  Chart,
+  GameStatus,
+  RhythmState,
+  InputEvent,
+  SystemUse,
+  HookRef,
+  Rule,
+  RhythmDef,
+} from "./types.js";
+export { hook, isHook } from "./types.js";
